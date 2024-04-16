265529a45badcf4558e5b9ffcb9536c113234cb7
Annika Nowak <annika.nowak@capgemini.com>
2024-04-16 14:01:54 +0200

    feat: add the AUTH_SERVICE and CUSTOM_URL to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,6 @@ export const environment = {
   KEYCLOAK_URL: 'http://keycloak-app/',
   KEYCLOAK_REALM: 'OneCX',
   skipRemoteConfigLoad: true,
+  AUTH_SERVICE: 'keycloak',
+  AUTH_SERVICE_CUSTOM_URL: 'http://keycloak-app/',
 };
